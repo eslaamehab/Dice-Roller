153eaa2157ad86dc8e18d61ce029c80ae2877816
eslaamehab <eslaam.ehaab@gmail.com>
2022-12-18 02:08:30 +0200

    Change number displayed on roll

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,16 +3,29 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // get SOMETHING
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.text = "Click to roll me"
+        rollButton.text = "Click to roll"
         rollButton.setOnClickListener {
-            Toast.makeText(this, "Rolling", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        // show a toast as click feedback
+        Toast.makeText(this, "Rolled", Toast.LENGTH_SHORT).show()
+        // get SOMETHING
+        val rollText: TextView = findViewById(R.id.roll_text)
+        // Generate a random number from 0 to 6
+        val randomInt = Random.nextInt(6) + 1
+        rollText.text = randomInt.toString()
     }
 }
